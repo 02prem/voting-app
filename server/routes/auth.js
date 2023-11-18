31284fc0d1c04194c7da6685e959f8a6bac34655
02prem <prem30102002@gmail.com>
2023-11-18 14:37:46 +0530

    Added authentication middleware jsonwebtokens

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,28 +1,23 @@
 const router = require('express').Router(); // import express and create instance of Router
 // const handle = require('../handlers');
 const db = require('../models');
+const jwt = require('jsonwebtoken');
 
-// router.get('/', async (req, res, next) => {
-//     try {
-//         const users = await db.User.find();
-  
-//         return res.status(200).json(users);
-//     } 
-//     catch (err) {
-//         return next({
-//             status: 400,
-//             message: err.message,
-//         });
-//     }
-// });
 
 router.post('/register', async function(req, res, next) {
     try{
         const user = await db.User.create(req.body);
         const {id, username} = user;
-        res.json({id, username});
+
+        const token = jwt.sign({id, username}, process.env.SECRET);
+
+        res.status(201).json({id, username, token});
     }
     catch(err){
+        if(err.code == 11000){
+            err.message = 'Sorry, this username is already taken';
+        }
+
         next(err);
     }
 });  // route for POST request to './register'
@@ -34,13 +29,16 @@ router.post('/login', async function(req, res, next) {
 
         const valid = await user.comparePassword(req.body.password);
         if(valid){
-            res.json({id, username});
+            const token = jwt.sign({id, username}, process.env.SECRET);
+
+            res.json({id, username, token});
         }
         else{
-            throw new Error('Invalid Username/password');
+            throw new Error();
         }
     }
     catch(err){
+        err.message = 'Invalid Username/password';
         next(err);
     }
 });
